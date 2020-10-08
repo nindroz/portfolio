be44a99d1487f7954a675d8c005ed189f03e4436
nindroz <mantha.hitesh@gmail.com>
2020-10-07 21:39:56 -0700

    playing around with anims and setup react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Project from "./Project.js";
 import Icons from "./Icons.js";
 import "./styling/App.css";
@@ -6,29 +8,36 @@ import "./styling/App.css";
 export default class App extends Component {
   constructor() {
     super();
-    this.state = {
-      title: "Hi I'm Hitesh Mantha",
-    };
   }
+
   render() {
     return (
       <div className="App">
-        <div className="swoosh"></div>
-        <h1 class="title">
-          <span style={{ color: "white" }}>Hi I'm</span>
-          <br/>
-          Hitesh Mantha
-        </h1>
-        <Icons/>
-        <button>water</button>
-        <div id="projects">
-          <Project />
-          <Project />
-          <Project />
-          <Project />
-          <Project />
-          <Project />
-        </div>
+        <Router>
+          <Switch>
+            <Route path="/test">
+              <div className="swoosh"></div>
+            </Route>
+            <Route path="/">
+              <div className="swoosh"></div>
+              <Link to="/test">test</Link>
+              <h1 class="title">
+                <span style={{ color: "white" }}>Hi I'm</span>
+                <br />
+                Hitesh Mantha
+              </h1>
+              <Icons />
+              <div id="projects">
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+                <Project />
+              </div>
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
