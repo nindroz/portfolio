80742c6fbd9c033191f612e2a3ab09402e119985
nindroz <mantha.hitesh@gmail.com>
2020-10-13 21:27:09 -0700

    cleaned up stuff

--- src/App.js
+++ src/App.js
@@ -1,44 +1,34 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Project from "./Project.js";
 import Icons from "./Icons.js";
 import "./styling/App.css";
 
 export default class App extends Component {
-  constructor() {
-    super();
-  }
+	constructor() {
+		super();
+	}
 
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Switch>
-            <Route path="/test">
-              <div className="swoosh"></div>
-            </Route>
-            <Route path="/">
-              <div className="swoosh"></div>
-              <Link to="/test">test</Link>
-              <h1 class="title">
-                <span style={{ color: "white" }}>Hi I'm</span>
-                <br />
-                Hitesh Mantha
-              </h1>
-              <Icons />
-              <div id="projects">
-                <Project />
-                <Project />
-                <Project />
-                <Project />
-                <Project />
-                <Project />
-              </div>
-            </Route>
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div className="swoosh"></div>
+				<h1 class="title">
+					<span style={{ color: "white" }}>Hi I'm</span>
+					<br />
+					Hitesh Mantha
+				</h1>
+				<Icons />
+				<div id="projects">
+					<Project />
+					<Project />
+					<Project />
+					<Project />
+					<Project />
+					<Project />
+				</div>
+				<h1 style={{ fontSize: 200 }}>ABOUT ME</h1>
+			</div>
+		);
+	}
 }
