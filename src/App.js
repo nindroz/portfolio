1ffa19e15678f1ef5852333f93e46e687c2fb7e1
nindroz <mantha.hitesh@gmail.com>
2020-10-13 22:09:30 -0700

    added bio and styled some more stuff

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
 		return (
 			<div className="App">
 				<div className="swoosh"></div>
-				<h1 class="title">
+				<h1 className="title">
 					<span style={{ color: "white" }}>Hi I'm</span>
 					<br />
 					Hitesh Mantha
@@ -27,7 +27,15 @@ export default class App extends Component {
 					<Project />
 					<Project />
 				</div>
-				<h1 style={{ fontSize: 200 }}>ABOUT ME</h1>
+				<h1 className="title2">ABOUT ME</h1>
+				<p id="block">
+					Hey there, Im hitesh. Im a 15 yr old software developer and
+					cybersecurity enthusiast that loves to make things whether that be
+					digital or physical. I'm also interested in mechanical keyboards and
+					3D printing. Feel free to contact me anywhere to just chat or work
+					with me on a project. For easy access my discord is{" "}
+					<b>Nindroz#3903</b>
+				</p>
 			</div>
 		);
 	}
