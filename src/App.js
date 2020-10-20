77b5d7744664acb7297352b128f556e41861e113
nindroz <mantha.hitesh@gmail.com>
2020-10-19 21:03:29 -0700

    added basic scrolling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Link, animateScroll as scroll } from "react-scroll";
 import Project from "./Project.js";
 import Icons from "./Icons.js";
 import "./styling/App.css";
@@ -12,48 +12,63 @@ export default class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<div className="swoosh"></div>
-				<h1 className="title">
-					<span style={{ color: "white" }}>Hi I'm</span>
-					<br />
-					Hitesh Mantha
-				</h1>
 				<Icons />
-				<div id="projects">
-					<Project />
-					<Project />
-					<Project />
-					<Project />
-					<Project />
-					<Project />
-				</div>
-				<h1 className="title2">
-					ABOUT <span style={{ color: "white" }}>ME</span>
-				</h1>
-				<p id="block">
-					Hey there &#128075; ,{" "}
-					<span
-						style={{
-							background: "white",
-							borderRadius: "5px",
-							opacity: 0.9,
-							paddingLeft: "5px",
-							paddingRight: "5px",
-						}}
+				<section id="main">
+					<Link
+						activeClass="active"
+						to="aboutMe"
+						spy={true}
+						smooth={true}
+						offset={100}
+						duration={500}
+						classID="main"
 					>
-						I'm Hitesh
-					</span>
-					. Im a 15 yr old software developer and cybersecurity enthusiast that
-					loves to make things and learn pretty much anything. I'm also
-					interested in mechanical keyboards and looking for other cool hobbies!
-					Feel free to contact me to just chat or work with me on a project. The
-					best way to reach me is on Discord{" "}
-					<span
-						style={{ background: "white", borderRadius: "5px", opacity: 0.9 }}
-					>
-						[Nindroz#3903]
-					</span>
-				</p>
+						Hey
+					</Link>
+					<div className="swoosh"></div>
+					<h1 className="title">
+						<span style={{ color: "white" }}>Hi I'm</span>
+						<br />
+						Hitesh Mantha
+					</h1>
+					<div id="projects">
+						<Project />
+						<Project />
+						<Project />
+						<Project />
+						<Project />
+						<Project />
+					</div>
+				</section>
+				<section id="aboutMe">
+					<h1 className="title2">
+						ABOUT <span style={{ color: "white" }}>ME</span>
+					</h1>
+					<p id="block">
+						Hey there &#128075; ,{" "}
+						<span
+							style={{
+								background: "white",
+								borderRadius: "5px",
+								opacity: 0.9,
+								paddingLeft: "5px",
+								paddingRight: "5px",
+							}}
+						>
+							I'm Hitesh
+						</span>
+						. Im a 15 yr old software developer and cybersecurity enthusiast
+						that loves to make things and learn pretty much anything. I'm also
+						interested in mechanical keyboards and looking for other cool
+						hobbies! Feel free to contact me to just chat or work with me on a
+						project. The best way to reach me is on Discord @{" "}
+						<span
+							style={{ background: "white", borderRadius: "5px", opacity: 0.9 }}
+						>
+							[Nindroz#3903]
+						</span>
+					</p>
+				</section>
 			</div>
 		);
 	}
