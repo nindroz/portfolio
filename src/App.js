83d8b3bf6c9d2af1ebd01a57e0faafa558c24b6d
nindroz <mantha.hitesh@gmail.com>
2020-10-29 22:10:01 -0700

    switched it out for locomotive scroll

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
 import ScrollTrigger from "react-scroll-trigger";
 import { endpoint, gqlQuery, dummyProject } from "./gql.js";
+import LocomotiveScroll from "locomotive-scroll";
 import Project from "./Project.js";
 import Icons from "./Icons.js";
 import "./styling/App.css";
@@ -11,7 +12,12 @@ export default class App extends Component {
 		super(props);
 		this.state = {};
 	}
+
 	componentDidMount() {
+		const scroll = new LocomotiveScroll({
+			el: document.querySelector("[data-scroll-container]"),
+			smooth: true,
+		});
 		endpoint().then((ret) => {
 			this.setState({
 				...ret.props.projects,
@@ -22,8 +28,8 @@ export default class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Icons />
-				<ScrollTrigger id="main">
+				<div data-scroll-container>
+					{/* <ScrollTrigger id="main">
 					<Link
 						activeClass="active"
 						to="aboutMe"
@@ -33,52 +39,69 @@ export default class App extends Component {
 						duration={500}
 					>
 						Hey
-					</Link>
+					</Link> */}
 					<div className="swoosh"></div>
-					<h1 className="title">
-						<span style={{ color: "white" }}>Hi I'm</span>
-						<br />
-						Hitesh Mantha
-					</h1>
-					<div id="projects">
-						{Object.values(this.state).map((data, key) => {
-							return (
-								<Project
-									name={JSON.stringify(data.node.name).replace(/"/g, "")}
-								/>
-							);
-						})}
+					<div data-scroll-section>
+						<Icons />
+						<h1 className="title">
+							<span style={{ color: "white" }}>Hi I'm</span>
+							<br />
+							Hitesh Mantha
+						</h1>
+						<div id="projects">
+							{Object.values(this.state).map((data, key) => {
+								return (
+									<Project
+										name={JSON.stringify(data.node.name).replace(/"/g, "")}
+										desc={JSON.stringify(data.node.description).replace(
+											/"/g,
+											""
+										)}
+										lang={JSON.stringify(
+											data.node.primaryLanguage.name
+										).replace(/"/g, "")}
+									/>
+								);
+							})}
+						</div>
+						{/* </ScrollTrigger>
+				<ScrollTrigger id="aboutMe"> */}
 					</div>
-				</ScrollTrigger>
-				<ScrollTrigger id="aboutMe">
-					<h1 className="title2">
-						ABOUT <span style={{ color: "white" }}>ME</span>
-					</h1>
-					<p id="block">
-						Hey there &#128075; ,{" "}
-						<span
-							style={{
-								background: "white",
-								borderRadius: "5px",
-								opacity: 0.9,
-								paddingLeft: "5px",
-								paddingRight: "5px",
-							}}
-						>
-							I'm Hitesh
-						</span>
-						. Im a 15 yr old software developer and cybersecurity enthusiast
-						that loves to make things and learn pretty much anything. I'm also
-						interested in mechanical keyboards and looking for other cool
-						hobbies! Feel free to contact me to just chat or work with me on a
-						project. The best way to reach me is on Discord @{" "}
-						<span
-							style={{ background: "white", borderRadius: "5px", opacity: 0.9 }}
-						>
-							[Nindroz#3903]
-						</span>
-					</p>
-				</ScrollTrigger>
+					<div data-scroll-section>
+						<h1 className="title2">
+							ABOUT <span style={{ color: "white" }}>ME</span>
+						</h1>
+						<p id="block">
+							Hey there &#128075; ,{" "}
+							<span
+								style={{
+									background: "white",
+									borderRadius: "5px",
+									opacity: 0.9,
+									paddingLeft: "5px",
+									paddingRight: "5px",
+								}}
+							>
+								I'm Hitesh
+							</span>
+							. Im a 15 yr old software developer and cybersecurity enthusiast
+							that loves to make things and learn pretty much anything. I'm also
+							interested in mechanical keyboards and looking for other cool
+							hobbies! Feel free to contact me to just chat or work with me on a
+							project. The best way to reach me is on Discord @{" "}
+							<span
+								style={{
+									background: "white",
+									borderRadius: "5px",
+									opacity: 0.9,
+								}}
+							>
+								[Nindroz#3903]
+							</span>
+						</p>
+					</div>
+					{/* </ScrollTrigger> */}
+				</div>
 			</div>
 		);
 	}
--- src/Project.js
+++ src/Project.js
@@ -7,12 +7,10 @@ export default class Project extends Component {
 		return (
 			<div className="element">
 				<h1>{this.props.name}</h1>
-				<p>
-					descriptionds dfsjkhsdfkhjsd jkdfshsfdkjhf hfhjddsfjkh kjhdfksdjhf
-				</p>
+				<p>{this.props.desc}</p>
 				<hr style={{ border: "solid 1px #242024" }}></hr>
 				<div className="underline">
-					<h2>JAVASCRIPT</h2>
+					<h2>{this.props.lang}</h2>
 					<FaGithub id="ghIcon" />
 				</div>
 			</div>
