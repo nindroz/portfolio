cdcb0b1420a72130c66ca1a05655560a8e44f2e7
nindroz <mantha.hitesh@gmail.com>
2020-11-09 20:54:06 -0800

    ipad res

--- src/Icons.js
+++ src/Icons.js
@@ -1,16 +1,34 @@
-import React, { Component } from 'react'
-import { FaGithubAlt, FaTwitter } from 'react-icons/fa'
-import { HiOutlineMail } from 'react-icons/hi'
-import './styling/Icons.css'
+import React, { Component } from "react";
+import { FaGithubAlt, FaTwitter } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import "./styling/Icons.css";
 
 export default class Icons extends Component {
-    render() {
-        return(<div className="iconPack">
-                <a href = "https://github.com/nindroz" target="_blank" rel="noopener noreferrer"><FaGithubAlt className="icon"/></a>
-                <a href = "https://twitter.com/Nindroz3" target="_blank" rel="noopener noreferrer"><FaTwitter className="icon"/></a>
-                <a href = "mailto:mantha.hitesh@gmail.com" target="_blank" rel="noopener noreferrer"><HiOutlineMail className="icon"/></a>
-               </div>
-        )
-    }
+	render() {
+		return (
+			<div className="iconPack">
+				<a
+					href="https://github.com/nindroz"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<FaGithubAlt className="icon" id="gh" />
+				</a>
+				<a
+					href="https://twitter.com/Nindroz3"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<FaTwitter className="icon" />
+				</a>
+				<a
+					href="mailto:mantha.hitesh@gmail.com"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<HiOutlineMail className="icon" />
+				</a>
+			</div>
+		);
+	}
 }
-
